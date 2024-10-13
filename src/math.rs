359875809e3f0c811e1bff2dbac3e6fb65f5e081
Morten Andersson <morten.andersson@gmail.com>
2024-10-13 12:57:22 +0200

    add math ops

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,96 @@
+use std::ops::{Add, Sub, Mul, Div};
 
-// Simple Vector3 and Matrix4 implementations
+pub trait Smoothstep {
+    fn smoothstep(self, edge0: Self, edge1: Self) -> Self;
+}
+
+impl Smoothstep for f32 {
+    fn smoothstep(self, edge0: f32, edge1: f32) -> f32 {
+        let t = ((self - edge0) / (edge1 - edge0)).clamp(0.0, 1.0);
+        t * t * (3.0 - 2.0 * t)
+    }
+}
+
+pub trait Clamp {
+    fn clamp(self, min: Self, max: Self) -> Self;
+}
+
+impl Clamp for f32 {
+    fn clamp(self, min: f32, max: f32) -> f32 {
+        if self < min { min }
+        else if self > max { max }
+        else { self }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn lerp<T: Into<Vec2>>(&self, other: T, t: f32) -> Self {
+        let other = other.into();
+        Self {
+            x: self.x + (other.x - self.x) * t,
+            y: self.y + (other.y - self.y) * t,
+        }
+    }
+
+    pub fn length(&self) -> f32 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    pub fn normalize(&self) -> Self {
+        let len = self.length();
+        if len != 0.0 {
+            *self * (1.0 / len)
+        } else {
+            *self
+        }
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self::new(self.x + other.x, self.y + other.y)
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self::new(self.x - other.x, self.y - other.y)
+    }
+}
+
+impl Mul<f32> for Vec2 {
+    type Output = Self;
+    fn mul(self, scalar: f32) -> Self {
+        Self::new(self.x * scalar, self.y * scalar)
+    }
+}
+
+impl Div<f32> for Vec2 {
+    type Output = Self;
+    fn div(self, scalar: f32) -> Self {
+        Self::new(self.x / scalar, self.y / scalar)
+    }
+}
+
+
+impl From<&Vec2> for Vec2 {
+    fn from(v: &Vec2) -> Self {
+        *v
+    }
+}
+
 #[derive(Clone, Copy)]
 pub struct Vec3 {
     pub x: f32,
@@ -16,8 +107,20 @@ impl Vec3 {
         Self::new(0.0, 0.0, 0.0)
     }
 
+    pub fn splat(v: f32) -> Self {
+        Self { x: v, y: v, z: v }
+    }
+
     pub fn dot(&self, other: Vec3) -> f32 {
         self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
     }
 
     pub fn length(&self) -> f32 {
@@ -26,7 +129,11 @@ impl Vec3 {
 
     pub fn normalize(&self) -> Self {
         let len = self.length();
-        Self::new(self.x / len, self.y / len, self.z / len)
+        if len != 0.0 {
+            *self * (1.0 / len)
+        } else {
+            *self
+        }
     }
 
     pub fn abs(&self) -> Self {
@@ -36,26 +143,165 @@ impl Vec3 {
     pub fn max(&self, other: Self) -> Self {
         Self::new(self.x.max(other.x), self.y.max(other.y), self.z.max(other.z))
     }
+
+    pub fn lerp<T: Into<Vec3>>(&self, other: T, t: f32) -> Self {
+        let other = other.into();
+        Self {
+            x: self.x + (other.x - self.x) * t,
+            y: self.y + (other.y - self.y) * t,
+            z: self.z + (other.z - self.z) * t,
+        }
+    }
+
+    pub fn sqrt(&self) -> Self {
+        Self {
+            x: self.x.sqrt(),
+            y: self.y.sqrt(),
+            z: self.z.sqrt(),
+        }
+    }
+
+    pub fn exp(&self) -> Self {
+        Self {
+            x: self.x.exp(),
+            y: self.y.exp(),
+            z: self.z.exp(),
+        }
+    }
+
+    pub fn fract(&self) -> Self {
+        Self {
+            x: self.x.fract(),
+            y: self.y.fract(),
+            z: self.z.fract(),
+        }
+    }
+
+    pub fn clamp(&self, min: f32, max: f32) -> Self {
+        Self {
+            x: self.x.clamp(min, max),
+            y: self.y.clamp(min, max),
+            z: self.z.clamp(min, max),
+        }
+    }
+
+    pub fn powf(&self, n: f32) -> Self {
+        Self {
+            x: self.x.powf(n),
+            y: self.y.powf(n),
+            z: self.z.powf(n),
+        }
+    }
 }
 
-impl std::ops::Add for Vec3 {
+impl Add for Vec3 {
     type Output = Self;
     fn add(self, other: Self) -> Self {
         Self::new(self.x + other.x, self.y + other.y, self.z + other.z)
     }
 }
 
-impl std::ops::Sub for Vec3 {
+impl Sub for Vec3 {
     type Output = Self;
     fn sub(self, other: Self) -> Self {
         Self::new(self.x - other.x, self.y - other.y, self.z - other.z)
     }
 }
 
-impl std::ops::Mul<f32> for Vec3 {
+impl Mul<f32> for Vec3 {
     type Output = Self;
     fn mul(self, scalar: f32) -> Self {
         Self::new(self.x * scalar, self.y * scalar, self.z * scalar)
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        Self::new(self.x * other.x, self.y * other.y, self.z * other.z)
+    }
+}
+
+impl From<&Vec3> for Vec3 {
+    fn from(v: &Vec3) -> Self {
+        *v
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Vec4 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+impl Vec4 {
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
+    }
+
+    pub fn splat(v: f32) -> Self {
+        Self::new(v, v, v, v)
+    }
+
+    pub fn min(&self, other: Self) -> Self {
+        Self::new(
+            self.x.min(other.x),
+            self.y.min(other.y),
+            self.z.min(other.z),
+            self.w.min(other.w),
+        )
+    }
+
+    pub fn lerp<T: Into<Vec4>>(&self, other: T, t: f32) -> Self {
+        let other = other.into();
+        Self {
+            x: self.x + (other.x - self.x) * t,
+            y: self.y + (other.y - self.y) * t,
+            z: self.z + (other.z - self.z) * t,
+            w: self.w + (other.w - self.w) * t,
+        }
+    }
+
+    pub fn sqrt(&self) -> Self {
+        Self {
+            x: self.x.sqrt(),
+            y: self.y.sqrt(),
+            z: self.z.sqrt(),
+            w: self.w.sqrt(),
+        }
+    }
+
+    pub fn length(&self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w).sqrt()
+    }
+
+    pub fn normalize(&self) -> Self {
+        let len = self.length();
+        if len != 0.0 {
+            *self * (1.0 / len)
+        } else {
+            *self
+        }
+    }
+}
+
+impl Mul<f32> for Vec4 {
+    type Output = Self;
+    fn mul(self, scalar: f32) -> Self {
+        Self::new(
+            self.x * scalar,
+            self.y * scalar,
+            self.z * scalar,
+            self.w * scalar
+        )
+    }
+}
+
+impl From<&Vec4> for Vec4 {
+    fn from(v: &Vec4) -> Self {
+        *v
     }
 }
 
@@ -90,7 +336,7 @@ impl Mat4 {
     }
 }
 
-impl std::ops::Mul for Mat4 {
+impl Mul for Mat4 {
     type Output = Self;
     fn mul(self, other: Self) -> Self {
         let mut result = [[0.0; 4]; 4];
